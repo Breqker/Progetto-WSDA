aee82bdadb56725dafe9197859b97d689e456d35
Breqker <gia.ferrara27@gmail.com>
2024-05-18 10:34:53 +0200

    codice

--- src/main/java/com/example/progettowsda/ProgettoWsdaApplication.java
+++ src/main/java/com/example/progettowsda/ProgettoWsdaApplication.java
@@ -18,7 +18,7 @@ public class ProgettoWsdaApplication {
 		@Override
 		public void addCorsMappings(CorsRegistry registry) {
 			registry.addMapping("/**")
-					.allowedOrigins("http://localhost:8000")
+					.allowedOrigins("http://localhost:8000", "http://localhost:8080")
 					.allowedMethods("GET", "POST", "PUT", "DELETE")
 					.allowCredentials(false)
 					.maxAge(3600);
