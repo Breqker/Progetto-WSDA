b1c04afd41b41d570a5b55aa36b2c9109113814a
eminem98 <carramusaeugenio@gmail.com>
2024-05-21 17:43:06 +0200

    Eliminato contenuto bottom dalla pagina. La mappa prende tutta la pagina. Contentuo top adesso visualizzabile cliccando all'interno del marker.

--- src/main/resources/static/Impianto/impianto.js
+++ src/main/resources/static/Impianto/impianto.js
@@ -5,23 +5,7 @@ document.addEventListener("DOMContentLoaded", function() {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-    // Funzione per caricare i dati degli impianti dal server e mostrarli nel div inferiore
-    function caricaDatiImpianti() {
-        fetch('http://localhost:8000/Progetto_WSDA_EE_war_exploded/getImpianti')
-            .then(response => response.json())
-            .then(data => {
-                console.log(data); // Debug
-                const impiantiDiv = document.getElementById('impiantiDiv');
-                data.forEach(impianto => {
-                    const p = document.createElement('p');
-                    p.textContent = `ID Impianto: ${impianto.idImpianto}, ID Palinsesto: ${impianto.idPalinsesto}, Latitudine: ${impianto.latitudine}, Longitudine: ${impianto.longitudine}`;
-                    impiantiDiv.appendChild(p);
-                });
-            })
-            .catch(error => console.error('Errore:', error));
-    }
-
-    // Funzione per caricare lo stato degli impianti dal server e mostrarli nel div superiore
+    // Funzione per caricare lo stato degli impianti dal server e mostrarli nel div superiore e sulla mappa
     function caricaStatusImpianti() {
         fetch('http://localhost:8000/Progetto_WSDA_EE_war_exploded/getStatusImpianti')
             .then(response => {
@@ -32,29 +16,23 @@ document.addEventListener("DOMContentLoaded", function() {
             })
             .then(data => {
                 console.log(data); // Debug
-                const topDiv = document.getElementById('top-div');
                 data.forEach(impianto_ws => {
-                    const p = document.createElement('p');
-                    p.textContent = `ID Impianto: ${impianto_ws.idImpianto}, ID Palinsesto: ${impianto_ws.idPalinsesto}, Latitudine: ${impianto_ws.latitudine}, Longitudine: ${impianto_ws.longitudine}, Attivo: ${impianto_ws.isActive}`;
-                    topDiv.appendChild(p);
-
                     // Aggiungi un marker per ogni impianto_ws sulla mappa
                     var icon = L.icon({
                         iconUrl: impianto_ws.isActive ? '/Impianto/Immagini/switch-on.png' : '/Impianto/Immagini/switch-off.png',
-                        iconSize: [64, 64],
-                        iconAnchor: [32, 64],
-                        popupAnchor: [0, -32]
+                        iconSize: [32, 32],
+                        iconAnchor: [16, 32],
+                        popupAnchor: [0, -16]
                     });
 
                     L.marker([impianto_ws.latitudine, impianto_ws.longitudine], { icon: icon })
                         .addTo(map)
-                        .bindPopup(impianto_ws.idImpianto);
+                        .bindPopup(`ID Impianto: ${impianto_ws.idImpianto}<br>ID Palinsesto: ${impianto_ws.idPalinsesto}<br>Latitudine: ${impianto_ws.latitudine}<br>Longitudine: ${impianto_ws.longitudine}<br>Attivo: ${impianto_ws.isActive}`);
                 });
             })
             .catch(error => console.error('Errore:', error));
     }
 
-    // Chiamate alle funzioni per caricare i dati e lo stato degli impianti
-    caricaDatiImpianti();
+    // Chiamata alla funzione per caricare lo stato degli impianti
     caricaStatusImpianti();
 });
