ad5b52a0f522823d20ab9a302922bc8540db6892
eminem98 <carramusaeugenio@gmail.com>
2024-05-28 17:51:17 +0200

    tabellone

---
+++ src/main/java/com/example/progettowsda/controller/TabelloneController.java
@@ -0,0 +1,25 @@
+package com.example.progettowsda.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class TabelloneController {
+
+    @GetMapping("/tabellone")
+    public String getTabellone(@RequestParam(name = "id_impianto", required = false) String codImpianto,
+                               @RequestParam(name = "ref_palinsesto", required = false) String codPalinsesto,
+                               Model model) {
+        // Logica per gestire codImpianto e codPalinsesto
+        if (codImpianto != null) {
+            model.addAttribute("codImpianto", codImpianto);
+        }
+        if (codPalinsesto != null) {
+            model.addAttribute("codPalinsesto", codPalinsesto);
+        }
+        // Restituisce il nome del template Thymeleaf (tabellone.html)
+        return "funzionalita_tabellone/tabellone";
+    }
+}
