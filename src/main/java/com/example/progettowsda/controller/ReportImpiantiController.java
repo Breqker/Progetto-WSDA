8068baf0fd51d451f7bba2e287b29f5697be91fc
Amedeo Dante Aldegheri <aaldegheri80@gmail.com>
2024-06-03 15:52:39 +0200

    pseudo codice segnalazioni

--- src/main/java/com/example/progettowsda/controller/ReportImpiantiController.java
+++ src/main/java/com/example/progettowsda/controller/ReportImpiantiController.java
@@ -76,4 +76,88 @@ public class ReportImpiantiController {
 
         return "report_impianti";
     }
+
+
+
+
+    /*
+
+        1. Fare una query segnalazioni
+           1.1 valori di ritorno
+               1.1.1 cod_impianto, cod_palinstesto, durata_visual, data_inserimento
+           1.2 realizzo corrispondenze tramite altra query con palinsesti tramite cod_palinstesto in chiave con id_palinsesto
+               1.2.1 ricavo "path" da cui estrapolo il cartellone di riferimento
+               1.2.2 da path prendo il nome del cartellone con una manipolazione di stringa e la uso come variabile di riferimento es. /Pannalle_animali/animali.html -> animali -> nome_caratteristico
+
+
+        2. Definisco un'entità "Impression"
+           2.1 attributi: cod_impianto, cod_palinsesto, data_inserimento ,durata_visual, nome_carretisco
+           2.2 opportuni set e get
+
+        3. Repository dell'entità impression
+
+
+        4. Metto i miei risultati su una qualche lista di Impression
+
+
+        5. Sulla base degli elemti della lista
+           5.1 prendo un nome caratterisco scorro tutti gli elementi con quel nome per ricavarmi tutte le impression per quella singola pubblictà -> "Impressioni totali"
+           5.2 prendo un intervallo di date e un nome caratteristico scorro la lista e trovo -> "Impressioni per quel periodo su quel cartellone"
+           5.3 prendo singolo giorno scorro -> "Cosa è successo in quel giorno"
+
+
+        CREATE TABLE segnalazioni (
+    id_segnalazione VARCHAR(50),
+    cod_impianto VARCHAR(50),
+    cod_palinsesto VARCHAR(50),
+    cod_cartellone VARCHAR(50),
+    durata_visual INT,
+    data_inserimento TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (cod_impianto, id_segnalazione),
+    FOREIGN KEY (cod_impianto) REFERENCES impianto(id_impianto)
+);
+
+
+CREATE TABLE palinsesti (
+    id_palinsesto VARCHAR(50) PRIMARY KEY,
+    path VARCHAR(255)
+);
+
+
+---------------------------------------------------
+
+Lato front end
+
+
+3 menù
+-"Impressioni totali" -> deve essere visualizato appena aperta la pagina con un grafico
+-"Impressioni per quel periodo su quel cartellone" -> menù con calenadrio, menù a tendina con scelta cartelloni (si fa con una query totale oppure scorrendo la lista di entità e popolo il mio menù a tendina)
+-"Cosa è successo in quel giorno" -> solo calendario e risultato di scorrimento lista con query generalista
+
+
+     */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
