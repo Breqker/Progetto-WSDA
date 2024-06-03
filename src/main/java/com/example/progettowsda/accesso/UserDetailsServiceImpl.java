672d65efe1b0ae2712fa48509d5ae23dad411c72
Amedeo Dante Aldegheri <aaldegheri80@gmail.com>
2024-06-03 09:48:07 +0200

    Login funzionante

---
+++ src/main/java/com/example/progettowsda/accesso/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.example.progettowsda.accesso;
+
+import com.example.progettowsda.accesso.MyUserDetails;
+import com.example.progettowsda.entity.User;
+import com.example.progettowsda.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.*;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+        User user = userRepository.getUserByUsername(username);
+
+        if (user == null) {
+            throw new UsernameNotFoundException("Could not find user");
+        }
+
+        return new MyUserDetails(user);
+    }
+
+}
